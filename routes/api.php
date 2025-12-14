f669eb474700ff34c8d826366d54529d453c3299
masumrahmanhasan <masumrahmanhasan@gmail.com>
2025-12-14 17:57:30 +0600

    added user details

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\API\V1\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:api');
 
 Route::post('/login', LoginController::class);
+
+Route::middleware('auth:api')->group(function () {
+   Route::get('/user', function (Request $request) {
+       return $request->user();
+   });
+});
+
