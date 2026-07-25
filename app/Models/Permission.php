c7a3327a0766d277fb5541fca08c58efc2665377
masumrahmanhasan <masumrahmanhasan@gmail.com>
2026-07-25 18:25:57 +0600

    feat: added roles and permissions table and models

---
+++ app/Models/Permission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read string $key
+ */
+#[Fillable(['name', 'key'])]
+class Permission extends Model
+{
+    //
+}
